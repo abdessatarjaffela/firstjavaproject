037eeb3c2195207588cc677b92e359ce3b9a1cc6
jaffela <jaffela2011@gmail.com>
2023-10-03 16:11:59 +0100

    adding files

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,41 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Animal lion =new Animal("Felidae", "Lion", 5, true);
-        Animal dog =new Animal("pet", "rex", 4, true);
+        Animal lion = new Animal("Felidae", "Lion", 5, true);
+        Animal dog = new Animal("Pet", "Rex", 4, true);
+        Animal elephant = new Animal("Elephantidae", "Elephant", 10, false);
+        Animal giraffe = new Animal("Giraffidae", "Giraffe", 7, false);
+        Animal tiger = new Animal("Felidae", "Tiger", 6, true);
+        Animal panda = new Animal("Ursidae", "Panda", 8, true);
+        Animal crocodile = new Animal("Crocodylidae", "Crocodile", 15, false);
         System.out.println(lion.isMammal);
         System.out.println(lion.name);
 
         Zoo myZoo = new Zoo("My Zoo", "My City", 25);
+        Zoo myZoooo = new Zoo("My Zoo2", "My City2", 25);
         myZoo.displayZoo();
         System.out.println("----------------------------");
         System.out.println(myZoo);
         System.out.println(myZoo.toString());
         System.out.println(myZoo.addAnimal(lion));
         System.out.println(myZoo.addAnimal(dog));
+        System.out.println(myZoooo.addAnimal(elephant));
+        System.out.println(myZoooo.addAnimal(giraffe));
+        System.out.println(myZoooo.addAnimal(tiger));
+        System.out.println(myZoooo.addAnimal(panda));
+        System.out.println(myZoooo.addAnimal(crocodile));
         myZoo.displayAnimals();
         System.out.println(myZoo.searchAnimal(dog));
         System.out.println(myZoo.removeAnimal(dog));
+        System.out.println(myZoo.isZooFull());
+        System.out.println("----------------------------");
+        Zoo result = Zoo.comparerZoo(myZoooo, myZoo);
+
+        if (result != null) {
+            System.out.println("The zoo with the most animals is: " + result.getName());
+        } else {
+            System.out.println("Both zoos have the same number of animals.");
+        }
 
             }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -10,6 +10,9 @@ public class Zoo {
         this.city = city;
 
     }
+    public String getName() {
+        return name;
+    }
     public void displayZoo() {
         System.out.println("Zoo Name: " + name);
         System.out.println("City: " + city);
@@ -64,6 +67,40 @@ public class Zoo {
         }
         return false;
     }
+    public boolean isZooFull() {
+        int numberOfAnimals = 0;
+        for (Animal animal : animals) {
+            if (animal != null) {
+                numberOfAnimals++;
+            }
+        }
+
+        return numberOfAnimals >= nbrCages;
+    }
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        int countZoo1 = z1.countAnimals();
+        int countZoo2 = z2.countAnimals();
+
+        if (countZoo1 > countZoo2) {
+            return z1;
+        } else if (countZoo2 > countZoo1) {
+            return z2;
+        } else {
+            return null;
+        }
+    }
+
+    private int countAnimals() {
+        int numberOfAnimals = 0;
+        for (Animal animal : animals) {
+            if (animal != null) {
+                numberOfAnimals++;
+            }
+        }
+        return numberOfAnimals;
+    }
+
 
 
 
